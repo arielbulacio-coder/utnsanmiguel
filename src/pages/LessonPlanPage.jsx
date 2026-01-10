40f118c7b46ed696906687e1037ff318dd77588f
Your Name <you@example.com>
2026-01-10 11:26:48 -0300

    Remove duplicate NavBar render in LessonPlanPage

--- src/pages/LessonPlanPage.jsx
+++ src/pages/LessonPlanPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import LessonPlanModule from '../components/academic/LessonPlanModule';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
 
 const LessonPlanPage = () => {
     return (
         <div className="page-container">
-            <NavBar />
-            <div className="content-wrap" style={{ paddingTop: '80px' }}>
+            <div className="content-wrap" style={{ paddingTop: '20px' }}>
                 <LessonPlanModule />
             </div>
-            <Footer />
         </div>
     );
 };
